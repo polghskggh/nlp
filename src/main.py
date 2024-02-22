4d181ce1f0b519b0c1cbf2990ba4a55f513788e1
benjamin <benjaminHucko@gmail.com>
2024-02-22 13:06:40 +0100

    setup

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
+import datasets
+
+training_file = "WASSA_train.csv"
+test_file = "WASSA_dev.csv"
+
+
 def main():
-    pass
+    data_files = {"train": training_file,
+                  "test": test_file}
+
+    data = datasets.load_dataset("csv", data_files=data_files,
+                                 delimiter=",",
+                                 encoding='utf-8')
+
+    print(data)
 
 
 if __name__ == '__main__':
