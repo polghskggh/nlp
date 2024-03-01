8ee3051b4bac04d4af0c5709d6f2bf559a6b4728
benjamin <benjaminHucko@gmail.com>
2024-03-01 16:07:01 +0100

    changed what happens after training

--- analysis/filter.r
+++ analysis/filter.r
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 library(superml)
-
+library(caTools)
 
 data_t <- read.csv("../data/train.csv", sep=";")
 # create a label encoder object
@@ -11,21 +11,24 @@ encoder <- LabelEncoder$new()
 encoder$fit(data_t$emotion)
 
 
-filter <- function(filename)
+filter <- function(filename, split)
 {
     data <- read.csv(paste("../data/", filename, ".csv", sep=""), sep=";")
     data$emotion <- encoder$transform(data$emotion)
 
     data <- data %>% select(essay, emotion) %>% rename(text = essay, labels = emotion)
 
-    write.table(data, file = paste("../data/", filename, "-clean.csv", sep=""), sep = ";",
+    if (split < 1)
+    {
+        mask <- sample.split(data[,1], split)
+        write.table(data[!mask, 1:2], file = paste("../data/val-clean.csv", sep=""), sep = "\t",
+                    quote = FALSE, row.names = FALSE)
+        data <- data[mask, 1:2]
+    }
+
+    write.table(data, file = paste("../data/", filename, "-clean.csv", sep=""), sep = "\t",
                 quote = FALSE, row.names = FALSE)
 }
 
-
-
-
-filter("test")
-filter("train")
-
-
+filter("test", 1)
+filter("train", 0.7)
--- src/main.py
+++ src/main.py
@@ -1,32 +1,53 @@
 from datasets import load_dataset
-from transformers import (Trainer, TrainingArguments, DataCollatorWithPadding, AutoTokenizer, AutoModelForSequenceClassification)
+import evaluate
+from transformers import (Trainer, TrainingArguments, DataCollatorWithPadding,
+                          AutoTokenizer, AutoModelForSequenceClassification)
+from sklearn.metrics import confusion_matrix
 
 
 def load_data():
     data_files = {
         "train": "../data/train-clean.csv",
         "test": "../data/test-clean.csv",
+        "validation": "../data/val-clean.csv",
     }
-    return load_dataset("csv", data_files=data_files, delimiter=';'),
+    return load_dataset("csv", data_files=data_files, delimiter='\t'),
 
 
 def tokenize(dataset, tokenizer):
     return tokenizer(dataset["text"])
 
 
+def test_model(model, dataset):
+    predictions = model.forward(dataset["text"]).logits.argmax(axis=1)
+    metrics = compute_metrics(zip(predictions, dataset["label"]))
+    confusion_matrix(dataset["label"], predictions)
+    return metrics, confusion_matrix
+
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = logits.argmax(axis=1)
+
+    metrics = [evaluate.load("accuracy"), evaluate.load("f1"), evaluate.load("precision"), evaluate.load("recall")]
+    comutations = [metric.compute(predictions, labels) for metric in metrics]
+
+    return comutations
+
+
 def main():
-    dataset = load_data()
-    print(dataset)
+    dataset = load_data()[0]
 
     pretrained_path = "google-bert/bert-base-cased"
     tokenizer = AutoTokenizer.from_pretrained(pretrained_path)
 
     training_args = TrainingArguments(
         output_dir="model/",
+        evaluation_strategy="epoch",
         learning_rate=1e-5,
         per_device_train_batch_size=9,
         per_device_eval_batch_size=9,
-        num_train_epochs=20
+        num_train_epochs=1
     )
 
     dataset = dataset.map(tokenize, batched=True, fn_kwargs={"tokenizer": tokenizer})
@@ -39,11 +60,14 @@ def main():
         model=model,
         args=training_args,
         train_dataset=dataset["train"],
-        eval_dataset=dataset["test"],
+        eval_dataset=dataset["validation"],
         tokenizer=tokenizer,
         data_collator=data_collator,
     )
     trainer.train()
+
+    scores, confusion_mat = test_model(trainer.model, dataset["test"])
+    print(scores, confusion_mat)
 
 
 if __name__ == '__main__':
