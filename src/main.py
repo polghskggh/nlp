532e0206eb4a142a2b832fe714b98070e36b65be
benjamin <benjaminHucko@gmail.com>
2024-04-05 14:18:00 +0200

    improved model

--- src/main.py
+++ src/main.py
@@ -12,11 +12,23 @@ if torch.backends.mps.is_available():
 elif torch.cuda.is_available():
     device = torch.device("cuda")
     print(device)
+elif torch.roc.is_available():
+    device = torch.device("roc")
+    print(device)
 else:
     device = torch.device("cpu")
     print(device)
 
 batch_size = 30
+
+
+def batch_pass(model, inputs):
+    outputs = []
+    for current in range(0, len(inputs["input_ids"]), batch_size):
+        outputs.append(model(inputs["input_ids"][current: current + batch_size],
+                             inputs["attention_mask"][current: current + batch_size]))
+        print(f"batch {current} passed")
+    return outputs
 
 
 def predict_classifier(input_batch):
@@ -25,44 +37,27 @@ def predict_classifier(input_batch):
 
     inputs = [context + question for context, question in zip(input_batch["context"], input_batch["question"])]
     inputs = tokenizer(inputs, truncation=True, padding=True, return_tensors="pt")
-    outputs = None
 
     with torch.no_grad():
-        for current in range(0, len(inputs["input_ids"]), batch_size):
-            if outputs is None:
-                outputs = model(inputs["input_ids"][current: current + batch_size],
-                                inputs["attention_mask"][current: current + batch_size]).logits.argmax(-1)
-            else:
-                outputs = torch.cat((outputs,
-                                     model(inputs["input_ids"][current: current + batch_size],
-                                           inputs["attention_mask"][current: current + batch_size]).logits.argmax(-1)), dim=0)
+        outputs = batch_pass(model, inputs)
 
-    labels = outputs
+    labels = torch.cat([output.logits for output in outputs]).argmax(dim=-1)
+
     return labels
 
 
 def predict_question_answer(input_batch): 
-    model = AutoModelForQuestionAnswering.from_pretrained("programs/nlp/checkpoint-7500-seq2seq")
-    tokenizer = AutoTokenizer.from_pretrained("programs/nlp/checkpoint-7500-seq2seq")
+    model = AutoModelForQuestionAnswering.from_pretrained("qa_model/checkpoint-5000")
+    tokenizer = AutoTokenizer.from_pretrained("qa_model/checkpoint-5000")
 
     inputs = tokenizer(input_batch["question"], input_batch["context"],
                        truncation=True, padding=True, return_tensors="pt")
-    outputs = None
-    answer_start_index = None
-    answer_end_index = None
-    with torch.no_grad():
-        for current in range(0, len(inputs["input_ids"]), batch_size):
-            outputs = model(inputs["input_ids"][current: current + batch_size],
-                            inputs["attention_mask"][current: current + batch_size])
-            if answer_start_index is None: 
-                answer_start_index = outputs.start_logits.argmax(-1)
-                answer_end_index = outputs.end_logits.argmax(-1)
-            else:
-                answer_start_index = torch.cat((answer_start_index, outputs.start_logits.argmax(-1)), dim=0)
-                answer_end_index = torch.cat((answer_end_index, outputs.end_logits.argmax(-1)), dim=0)
 
-    answer_start_index = outputs.start_logits.argmax(-1)
-    answer_end_index = outputs.end_logits.argmax(-1)
+    with torch.no_grad():
+        outputs = batch_pass(model, inputs)
+
+    answer_start_index = torch.cat([output.start_logits for output in outputs]).argmax(dim=-1)
+    answer_end_index = torch.cat([output.end_logits for output in outputs]).argmax(dim=-1)
 
     answers = [tokenizer.decode(inputs.input_ids[0, start: end + 1])
                for start, end in zip(answer_start_index, answer_end_index)]
@@ -70,20 +65,23 @@ def predict_question_answer(input_batch):
 
 
 def predict(input_batch):
-    answers = predict_question_answer(input_batch)
-    print("answers ready")
     labels = predict_classifier(input_batch)
-
+    print("classifier done")
+    answers = predict_question_answer(input_batch)
+    print("answering done")
+    print(labels.shape)
     ret = {key: answer if label.item() else "" for key, label, answer in zip(input_batch["id"], labels, answers)}
     return json.dumps(ret)
 
 
 def main():
-    validation_data = load_dataset("squad_v2", split="validation")
+    validation_data = load_dataset("squad_v2", split="validation[:100]")
     print("dataset_loaded")
+    results = predict(validation_data)
+
     with open("output", 'w', encoding='utf-8') as f:
-        print(predict(validation_data), file=f)
+        print(results, file=f)
 
 
 if __name__ == '__main__':
-    train_seq2seq()
+    main()
